d8ec2fed2b768bb9b3f52fb193e5821a4e40fc68
DemLoveSky <dem.loginov@gmail.com>
2021-03-26 16:25:03 +0500

    added model "News"

--- myapp/news/models.py
+++ myapp/news/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
+
+
+
+    # id - INT
+    # title - varchar
+    # content - text
+    # created_at - DateTime
+    # updated-at - DateTime
+    # photo - Image
+    # is_published - Boolean
